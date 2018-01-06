6bcc5031067dbc01b8c24b8fab4d8e20a652a8e3
Douglas Joseph Pecot <djpecot@gmail.com>
2018-01-06 21:05:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,6 +8,6 @@ function happyHolidaysTo(name){
   return `Happy holidays, ${name}!`
 }
 
-function happyHolidayTo(holiday,name){
-  `Happy ${holiday}, ${name}!`
+function happyHolidayTo(holiday, name){
+  `Happy ${holiday},${name}!`
 }
